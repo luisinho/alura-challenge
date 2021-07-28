41788c4dc1e7af0aae6551de6af9968145373a16
luis3977@gmail.com <luis3977@gmail.com>
2021-07-27 23:47:57 -0300

    Criado projeto

---
+++ backend/src/main/java/br/com/alura/alurafix/AlurafixApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.alurafix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlurafixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlurafixApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/alura/alurafix/AlurafixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.alurafix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlurafixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
