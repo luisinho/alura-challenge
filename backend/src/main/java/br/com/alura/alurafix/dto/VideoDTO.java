6f4520ecb627bda15e1807d9ff69b1c28ff30a2e
luis3977@gmail.com <luis3977@gmail.com>
2021-07-28 21:05:42 -0300

    Criado Entity e DTO de Video

---
+++ backend/src/main/java/br/com/alura/alurafix/dto/VideoDTO.java
@@ -0,0 +1,81 @@
+package br.com.alura.alurafix.dto;
+
+import java.io.Serializable;
+
+public class VideoDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String titulo;
+	private String descricao;
+	private String url;
+
+	public VideoDTO() {
+
+	}
+
+	public VideoDTO(Long id, String titulo, String descricao, String url) {
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.url = url;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		VideoDTO other = (VideoDTO) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}	
+}
